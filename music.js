16b27b0d60a41ede5831b28e4831759094e1c043
Mansi <96160685+mansi2402@users.noreply.github.com>
2022-02-01 21:13:44 +0530

    Add files via upload

---
+++ music.js
@@ -0,0 +1,44 @@
+function play1()
+{
+    var audie = document.getElementById("myAudio");
+    audie.src = ("bubbles.mp3");
+    audie.play();
+}
+
+function play2()
+{
+    var audie = document.getElementById("myAudio");
+    audie.src = ("clay.mp3");
+    audie.play();
+}
+function play3()
+{
+    var audie = document.getElementById("myAudio");
+    audie.src = ("confetti.mp3");
+    audie.play();
+}
+
+function play4()
+{
+    var audie = document.getElementById("myAudio");
+    audie.src = ("glimmer.mp3");
+    audie.play();
+}
+function play5()
+{
+    var audie = document.getElementById("myAudio");
+    audie.src = ("moon.mp3");
+    audie.play();
+}
+function play6()
+{
+    var audie = document.getElementById("myAudio");
+    audie.src = ("ufo.mp3");
+    audie.play();
+}
+function play7()
+{
+    var audie = document.getElementById("myAudio");
+    audie.src = ("alram.wav");
+    audie.play();
+}
